bb92f2981d43e9d8d6fb53b06d578508b7c8b981
saurabh <saurabh2836@gmail.com>
2015-09-29 02:29:11 +0530

    add mantis to git Signed-off-by: saurabh <saurabh2836@gmail.com>

---
+++ config_inc.php
@@ -0,0 +1,10 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'bugtracker';
+	$g_db_username = 'root';
+	$g_db_password = '';
+        
+//        $g_css_include_file ='/colorized/default.css';
+//        $g_css_include_file ='/rockerstar/default.css'
+?>
